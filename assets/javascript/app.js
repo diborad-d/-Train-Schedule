696b8df22aaa734f211fb7db3856e3f615e2f606
Dibora D <diborad@yahoo.com>
2019-09-26 15:26:04 -0500

    added ticket page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,38 @@
-$(document).ready(function() {
-  $("<div>").button.moment().format("LLLL");
+// Or with jQuery
+const firebaseConfig = {
+  apiKey: "AIzaSyB6UPhytJViM7sRJ-l0o9oJVNwmw497pqo",
+  authDomain: "train-schedule-34437.firebaseapp.com",
+  databaseURL: "https://train-schedule-34437.firebaseio.com",
+  projectId: "train-schedule-34437",
+  storageBucket: "",
+  messagingSenderId: "739886981960",
+  appId: "1:739886981960:web:3ce4e63e293725b8b2c97d"
+};
+firebase.initializeApp(firebaseConfig);
 
 let database = firebase.database();
-$("#submit").on("click", function() {
-  event.preventDefault();
-  let departure = $(".departure").val().trim();
-  let arrival = $(".arrival").val().trim();
-//   let startDate = $("#startDate").val();
-//   let monthlyRate = $("#monthlyRate").val().trim();
-//   //
-  database.ref().push({
-    name: departure,
-    role: role,
-    startDate: startDate,
-    monthlyRate: monthlyRate
-  });
+var ref = firebase.database().ref();                           
+ref.on("value", function(snapshot){
+    output.innerHTML = JSON.stringify(snapshot.val(), null, 2);
 });
-});
+// $(document).ready(function() {
+//   let trainSchedue = {
+//     arrival: 0,
+//     departure: 0,
+//     firstTrain: 0,
+//     lastTrain: 0,
+//     firstTrain: 0,
+//     arriving: 0,
+//     leaving: 0
+//   };
+
+//   $(document).on("click", function() {
+//       let arrival = $("#arrival").val().trim();
+//       let departure = $("#departure").val().trim();
+//       trainSchedue.arrival = arrival;
+//       trainSchedue.departure = departure;
+//     database.ref().push({
+//       trainSchedue
+//     });
+//   });
+// });
